cfc501c2fac3c8168aa110410163fb4b2209d65f
fernandohre <senhor.clinton@gmail.com>
2021-05-04 22:24:07 -0300

    inicio componente contador

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <contador />
+  </div>
+</template>
+
+<script>
+import Contador from './components/Contador.vue'
+export default {
+  name: 'App',
+  components: {
+    Contador
+  }
+}
+</script>
+
+<style>
+#app {
+  
+}
+</style>
